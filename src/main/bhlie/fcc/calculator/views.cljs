41398706d7821f7bd5cc941914dd23942f6bd7c3
Ben Lieberman <bhlieberman93@gmail.com>
2023-07-27 11:42:11 -0600

    calculator css fixed and looking good

--- src/main/bhlie/fcc/calculator/views.cljs
+++ src/main/bhlie/fcc/calculator/views.cljs
@@ -9,25 +9,23 @@
   [:div#calculator-display
    [:span#current-value @(subscribe [:calculator/current-value])]])
 
-(defn calculator [] 
+(defn calculator []
   [:div#calculator
    [current-value]
-   [:table
-    [:tbody
-     [:tr
-      [:td.calculator-button {:on-click click-to-update} 7] 
-      [:td.calculator-button {:on-click click-to-update} 8]
-      [:td.calculator-button {:on-click click-to-update} 9]
-      [:td.calculator-button {:on-click click-to-update} "-"]]
-     [:tr
-      [:td.calculator-button {:on-click click-to-update} 4]
-      [:td.calculator-button {:on-click click-to-update} 5]
-      [:td.calculator-button {:on-click click-to-update} 6]
-      [:td.calculator-button {:on-click click-to-update} "+"]]
-     [:tr
-      [:td.calculator-button {:on-click click-to-update} 1]
-      [:td.calculator-button {:on-click click-to-update} 2]
-      [:td.calculator-button {:on-click click-to-update} 3]]
-     [:tr
-      [:td.calculator-button {:on-click click-to-update} 0]
-      [:td.calculator-button {:on-click click-to-update} "."]]]]])
+   [:div#numbers
+    (for [i [{:el "AC" :id "clear"}
+             {:el "/" :id "divide"}
+             {:el "x" :id "mul"}
+             7 8 9
+             {:el "-" :id "minus"}
+             4 5 6
+             {:el "+" :id "plus"}
+             1 2 3
+             {:el "=" :id "equals"}
+             0
+             {:el "." :id "decimal"}]]
+      (condp = (type i)
+        js/Number [:div.calculator-button {:on-click click-to-update} i]
+        cljs.core/PersistentArrayMap
+        [:div.calculator-button {:on-click click-to-update
+                                 :id (:id i)} (:el i)]))]])
